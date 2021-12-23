aba12d63cd13ea2ef673ef6e69d77d5d3ae0fe1b
Martijn de Vos <mdmartijndevos@gmail.com>
2021-12-23 23:02:05 +0100

    Optimized + added day 2

--- day23_2.py
+++ day23_2.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-NUM_FISHES = 8
+NUM_FISHES = 16
 
 
 class Space:
@@ -18,22 +18,16 @@ class Space:
     def is_in_front_of_door(self):
         return self.num in [3, 5, 7, 9]
 
-    def get_adj_room(self):
-        for adj in self.adjacent:
-            if adj.is_room:
-                return adj
-        return None
-
     def __str__(self):
         return "Space %d" % self.num
 
 
 def get_fish_type(fish_ind):
-    if fish_ind in [0, 1]:
+    if fish_ind in [0, 1, 2, 3]:
         return 1
-    elif fish_ind in [2, 3]:
+    elif fish_ind in [4, 5, 6, 7]:
         return 2
-    elif fish_ind in [4, 5]:
+    elif fish_ind in [8, 9, 10, 11]:
         return 3
     return 4
 
@@ -41,26 +35,26 @@ def get_fish_type(fish_ind):
 def encode_state(position):
     hash_sum = 0
     for ind in range(1, len(position) + 1):
-        hash_sum += position[ind - 1] * (20 ** ind)
+        hash_sum += position[ind - 1] * (30 ** ind)
     return hash_sum
 
 
 def decode_state(hash_sum, num_items):
     values = []
     for ind in range(num_items, 0, -1):
-        values.append(hash_sum // (20 ** ind))
-        hash_sum %= 20 ** ind
+        values.append(hash_sum // (30 ** ind))
+        hash_sum %= 30 ** ind
     return tuple(values[::-1])
 
 
-target_rooms_for_fish_types = {1: [12, 13], 2: [14, 15], 3: [16, 17], 4: [18, 19]}
-#start_positions = (14, 13, 12, 15, 16, 17, 18, 19)  # Positions of a1, a2, b1, b2, c1, c2, d1, d2
-start_positions = (13, 19, 12, 16, 14, 17, 15, 18)  # Positions of a1, a2, b1, b2, c1, c2, d1, d2
-#start_positions = (15, 16, 14, 19, 13, 18, 12, 17)  # Positions of a1, a2, b1, b2, c1, c2, d1, d2
+target_rooms_for_fish_types = {1: [12, 13, 14, 15], 2: [16, 17, 18, 19], 3: [20, 21, 22, 23], 4: [24, 25, 26, 27]}
+#start_positions = (15, 22, 25, 27, 12, 18, 20, 21, 16, 17, 23, 26, 13, 14, 19, 24)
+#start_positions = (12, 17, 14, 15, 16, 13, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27)
+start_positions = (19, 20, 22, 25, 16, 18, 21, 27, 15, 17, 24, 26, 12, 13, 14, 23)
 
 # Prepare the field by creating spaces and connecting them
 nodes = {}
-for node_ind in range(1, 20):
+for node_ind in range(1, 28):
     node = Space(node_ind, is_room=(node_ind >= 12))
     if node_ind >= 12:
         node.is_room = True
@@ -68,12 +62,19 @@ for node_ind in range(1, 20):
 
 for node_ind in range(1, 11):  # Connect hallway
     Space.connect(nodes[node_ind], nodes[node_ind + 1])
-for node_ind in [3, 5, 7, 9]:  # Connect hallway <-> room
-    Space.connect(nodes[node_ind], nodes[node_ind + 9])
-for node_ind in [12, 14, 16, 18]:  # Connect room <-> room
-    Space.connect(nodes[node_ind], nodes[node_ind + 1])
 
-min_score = 100000000
+# Connect hallway <-> room
+Space.connect(nodes[3], nodes[12])
+Space.connect(nodes[5], nodes[16])
+Space.connect(nodes[7], nodes[20])
+Space.connect(nodes[9], nodes[24])
+
+for node_ind in [12, 16, 20, 24]:  # Connect room <-> room
+    Space.connect(nodes[node_ind], nodes[node_ind + 1])
+    Space.connect(nodes[node_ind + 1], nodes[node_ind + 2])
+    Space.connect(nodes[node_ind + 2], nodes[node_ind + 3])
+
+min_score = 100000000000
 step = 0
 
 evaluated = {}
@@ -97,18 +98,23 @@ while queue:
     # For each fish, find the possible movements and add them to the queue
     for fish_ind in range(0, NUM_FISHES):
         #print("Looking at movements of fish %d (cur pos: %d)" % (fish_ind, positions[fish_ind]))
+
         is_in_right_room = positions[fish_ind] in target_rooms_for_fish_types[get_fish_type(fish_ind)]
         if is_in_right_room:
-            # Check if we are in the right room and should move.
-            our_target_rooms = target_rooms_for_fish_types[get_fish_type(fish_ind)]
-            if positions[fish_ind] == our_target_rooms[1]:
-                continue  # We are at the lower half - don't move, we're fine
-            elif positions[fish_ind] == our_target_rooms[0] and our_target_rooms[1] in positions:
-                # Only move if the fish below us is conflicting
-                other_fish_type = get_fish_type(positions.index(our_target_rooms[1]))
-                if other_fish_type == get_fish_type(fish_ind):
-                    # Other fish is of the same type - we're good.
-                    continue
+            # Check the fishes 'below' us
+            is_ok = True
+            cur_room = positions[fish_ind] + 1
+            while cur_room not in [16, 20, 24, 28]:
+                if cur_room in positions:
+                    # Occupied room
+                    other_fish_type = get_fish_type(positions.index(cur_room))
+                    if other_fish_type != get_fish_type(fish_ind):
+                        is_ok = False
+                        break
+                cur_room += 1
+
+            if is_ok:
+                continue
 
         fish_pos_queue = [(positions[fish_ind], [positions[fish_ind]])]
         while fish_pos_queue:
@@ -138,15 +144,24 @@ while queue:
                     if adj_space.num not in target_rooms_for_fish_types[get_fish_type(fish_ind)]:
                         will_stop = False
 
-                    if adj_space.num in [12, 14, 16, 18] and (adj_space.num + 1) not in positions:
+                    if adj_space.num in [12, 16, 20, 24] and (adj_space.num + 1) not in positions:
+                        will_stop = False
+                    if adj_space.num in [13, 17, 21, 25] and (adj_space.num + 1) not in positions:
+                        will_stop = False
+                    if adj_space.num in [14, 18, 22, 26] and (adj_space.num + 1) not in positions:
                         will_stop = False
 
                     # check if there is no 'conflicting' fish in the adjacent room
-                    adj_room = adj_space.get_adj_room()
-                    if adj_room.num in positions:
-                        occupied_fish_type = get_fish_type(positions.index(adj_room.num))
-                        if occupied_fish_type != get_fish_type(fish_ind):
-                            will_stop = False
+                    cur_room = adj_space.num + 1
+                    while cur_room not in [16, 20, 24, 28]:
+                        if cur_room in positions:
+                            # Occupied room
+                            other_fish_type = get_fish_type(positions.index(cur_room))
+                            if other_fish_type != get_fish_type(fish_ind):
+                                will_stop = False
+                                break
+                        cur_room += 1
+
                 if not nodes[positions[fish_ind]].is_room and not adj_space.is_room:  # Rule 3
                     will_stop = False
 
@@ -158,23 +173,7 @@ while queue:
                     additional_score = len(visited) * (10 ** (get_fish_type(fish_ind) - 1))
                     new_score = cur_score + additional_score
 
-                    # Determine if we should evaluate this state - only do so if we
-                    # a) have not determined a state with a lower score before
-                    # b)
                     should_add = True
-                    # if new_positions in evaluated:
-                    #     # We evaluated this state before
-                    #     evaluated_score = evaluated[new_positions]
-                    #     if evaluated_score <= cur_score:
-                    #         should_add = False
-
-                    # for qind in range(len(queue)):
-                    #     qpos, qscore = queue[qind]
-                    #     if qpos == new_positions:
-                    #         if qscore <= new_score:  # There is already an item with a lower score in the queue
-                    #             should_add = False
-                    #             break
-
                     new_state = encode_state(new_positions)
                     if new_state in queue_entry_map:
                         # We already have an entry like this in the queue - if the score in the queue is higher, replace the queue score with the new (lower) score
@@ -189,7 +188,5 @@ while queue:
                         queue_entry_map[new_state] = queue_entry
 
     step += 1
-    # if step == 1:
-    #     break
     if step % 1000 == 0:
         print(len(queue))
